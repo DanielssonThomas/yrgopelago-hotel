45b939ae184869213c32e3ec6b4ddcd597a1f254
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2022-12-20 20:31:26 +0200

    fix: updated calendar book function

--- PHP/calendars.php
+++ PHP/calendars.php
@@ -4,38 +4,53 @@ require 'vendor/autoload.php';
 
 use benhall14\phpCalendar\Calendar as Calendar;
 
-$BudgetBookings = $dbh->query('SELECT arrival_date, departure_date FROM bookings WHERE room_id = 1');
-$StandardBookings = $dbh->query('SELECT arrival_date, departure_date FROM bookings WHERE room_id = 2');
-$LuxuryBookings = $dbh->query('SELECT arrival_date, departure_date FROM bookings WHERE room_id = 3');
-
-$BudgetBookedDates = $BudgetBookings->fetchAll(PDO::FETCH_ASSOC);
-$StandardBookedDates = $StandardBookings->fetchAll(PDO::FETCH_ASSOC);
-$LuxuryBookedDates = $LuxuryBookings->fetchAll(PDO::FETCH_ASSOC);
+$dates = $dbh->query('SELECT room_id, arrival_date, departure_date FROM bookings');
+$data = $dates->fetchAll(PDO::FETCH_ASSOC);
 
 $budgetEvents = array();
 $StandardEvents = array();
+$luxuryEvents = array();
 
-foreach ($BudgetBookedDates as $date) {
-    $budgetEvents[] = array(
-        'start' => $date['arrival_date'],
-        'end' => $date['departure_date'],
-        'mask' => true,
-    );
+
+foreach ($data as $room) {
+    if ($room['room_id'] === 1) {
+        $budgetEvents[] = array(
+            'start' => $room['arrival_date'],
+            'end' => $room['departure_date'],
+            'mask' => true,
+        );
+    }
+
+    if ($room['room_id'] === 2) {
+        $StandardEvents[] = array(
+            'start' => $room['arrival_date'],
+            'end' => $room['departure_date'],
+            'mask' => true,
+        );
+    }
+
+    if ($room['room_id'] === 3) {
+        $luxuryEvents[] = array(
+            'start' => $room['arrival_date'],
+            'end' => $room['departure_date'],
+            'mask' => true,
+        );
+    }
 }
-foreach ($StandardEvents as $date) {
-    $StandardEvents[] = array(
-        'start' => $date['arrival_date'],
-        'end' => $date['departure_date'],
-        'mask' => true,
-    );
-}
 
-
-$budgetCalendar = (new Calendar);
-$budgetCalendar->addTableClasses('calendar-table');
+$budgetCalendar = new Calendar;
 $budgetCalendar->addEvents($budgetEvents);
+
 $standardCalendar = new Calendar;
+$standardCalendar->addEvents($StandardEvents);
+
 $luxuryCalendar = new Calendar;
+$luxuryCalendar->addEvents($luxuryEvents);
 
 $budgetCalendar->useMondayStartingDate();
+$standardCalendar->useMondayStartingDate();
+$luxuryCalendar->useMondayStartingDate();
+
 echo $budgetCalendar->display(date('2023-01-01'));
+echo $standardCalendar->display(date('2023-01-01'));
+echo $luxuryCalendar->display(date('2023-01-01'));
