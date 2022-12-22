fd037bea9f3b14950534e5f535c6973c528ce8d0
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2022-12-22 20:06:11 +0200

    fix: fixed booking response to js

--- JS/booking.js
+++ JS/booking.js
@@ -1,12 +1,23 @@
 bookingForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const formData = new FormData();
+  const formData = new FormData(bookingForm);
+  const data = [];
 
   fetch('PHP/booking.php', {
     method: 'POST',
     body: formData,
-  })
-    .then((response) => response.json())
-    .then(console.log);
+  }).then((response) => response.json());
+
+  //   if (!$data['is_booking_available']) {
+  //     const div = document.createElement('div');
+  //     div.classList.add('form-err');
+  //     bookingForm.appendChild(div);
+  //   }
+
+  //   if (!$data['is_transferCode_valid']) {
+  //     const div = document.createElement('div');
+  //     div.classList.add('form-err');
+  //     bookingForm.appendChild(div);
+  //   }
 });
--- PHP/booking.php
+++ PHP/booking.php
@@ -1,41 +1,104 @@
 <?php
 
 declare(strict_types=1);
+require(__DIR__ . '/hotelFunctions.php');
+// $_POST['transferCode']
 
-$responseData = file_get_contents('../response.json');
-$responseData = json_decode($responseData, true);
-$status = array('is_booking_available' => false, 'is_transferCode_valid' => false, 'totalCost' => 0);
-header('content-type: application/json');
+// $responseData = file_get_contents('../response.json');
+// $responseData = json_decode($responseData, true);
+// $status = array('is_booking_available' => false, 'is_transferCode_valid' => false, 'totalCost' => 0);
 
-if (isset($_POST['room'], $_POST['arrivalDate'], $_POST['departureDate'], $_POST['transferCode'], $_POST['features'])) {
+// $room = 'budget';
+// $arrivalDate = '2023-01-02';
+// $departureDate = '2023-01-04';
+// // $transferCode = $_POST['transferCode'];
+// $features = array('feature-sauna', 'feature-room');
+
+// if ($room === "budget") {
+//     $room = 1;
+//     $status['totalCost'] += 1;
+// } else if ($room === "standard") {
+//     $room = 2;
+//     $status['totalCost'] += 2;
+// } else if ($room === "luxury") {
+//     $room = 3;
+//     $status['totalCost'] += 3;
+// }
+
+// foreach ($features as $feature) {
+//     if ($feature === 'feature-sauna') {
+//         $status['totalCost'] += 2;
+//     }
+
+//     if ($feature === 'feature-tour') {
+//         $status['totalCost'] += 3;
+//     }
+
+//     if ($feature === 'feature-room') {
+//         $status['totalCost'] += 2;
+//     }
+// }
+
+// if (isBookingAvailable($room, $arrivalDate, $departureDate)) {
+//     $status['is_booking_available'] = true;
+// }
+
+// // if (isTransferCodeValid($transferCode, $price)) {
+// //     $status['is_transferCode_valid'] = true;
+// // }
+
+// // if ($status['is_booking_available'] && $status['is_transferCode_valid']) {
+// //     book($room, $arrivalDate, $departureDate);
+// //     cashInTransferCode($transferCode);
+// // }
+
+// if ($status['is_booking_available']) {
+//     book($room, $arrivalDate, $departureDate);
+//     $status['is_transferCode_valid'] = true;
+// }
+
+// // array_push($responseData, $status);
+// // $responseData = json_encode($responseData);
+// // file_put_contents('../response.json', $responseData);
+// header('Content-Type: application/json');
+// echo json_encode($status);
+
+if (isset($_POST['room'], $_POST['arrivalDate'], $_POST['departureDate'])) {
+
+    $responseData = file_get_contents('../response.json');
+    $responseData = json_decode($responseData, true);
+    $status = array('is_booking_available' => false, 'is_transferCode_valid' => false, 'totalCost' => 0);
+
     $room = $_POST['room'];
     $arrivalDate = $_POST['arrivalDate'];
     $departureDate = $_POST['departureDate'];
-    $transferCode = $_POST['transferCode'];
-    $features = $_POST['features'];
+    // $transferCode = $_POST['transferCode'];
 
     if ($room === "budget") {
         $room = 1;
-        $status['totalCost'] + 1;
+        $status['totalCost'] += 1;
     } else if ($room === "standard") {
         $room = 2;
-        $status['totalCost'] + 2;
+        $status['totalCost'] += 2;
     } else if ($room === "luxury") {
         $room = 3;
-        $status['totalCost'] + 3;
+        $status['totalCost'] += 3;
     }
 
-    foreach ($features as $feature) {
-        if ($feature === 'feature-sauna') {
-            $status['totalCost'] + 2;
-        }
+    if (isset($_POST['features'])) {
+        $features = $_POST['features'];
+        foreach ($features as $feature) {
+            if ($feature === 'feature-sauna') {
+                $status['totalCost'] += 2;
+            }
 
-        if ($feature === 'feature-tour') {
-            $status['totalCost'] + 3;
-        }
+            if ($feature === 'feature-tour') {
+                $status['totalCost'] += 3;
+            }
 
-        if ($feature === 'feature-room') {
-            $status['totalCost'] + 2;
+            if ($feature === 'feature-room') {
+                $status['totalCost'] += 2;
+            }
         }
     }
 
@@ -43,14 +106,23 @@ if (isset($_POST['room'], $_POST['arrivalDate'], $_POST['departureDate'], $_POST
         $status['is_booking_available'] = true;
     }
 
-    if (isTransferCodeValid($transferCode, $price)) {
+    // if (isTransferCodeValid($transferCode, $price)) {
+    //     $status['is_transferCode_valid'] = true;
+    // }
+
+    // if ($status['is_booking_available'] && $status['is_transferCode_valid']) {
+    //     book($room, $arrivalDate, $departureDate);
+    //     cashInTransferCode($transferCode);
+    // }
+
+    if ($status['is_booking_available']) {
+        book($room, $arrivalDate, $departureDate);
         $status['is_transferCode_valid'] = true;
     }
 
-    if ($status['is_booking_available'] && $status['is_transferCode_valid']) {
-        book($room, $arrivalDate, $departureDate);
-        cashInTransferCode($transferCode);
-    }
-
+    // array_push($responseData, $status);
+    // file_put_contents('../response.json', $responseData);
+    // $responseData = json_encode($responseData);
+    header('Content-type: application/json');
     echo json_encode($status);
 }
